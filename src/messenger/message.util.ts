a58d984feaa152c0389b7abc12e34f84cc4c15b7
Gregi <gregi.stuber@gmail.com>
2024-01-28 22:44:36 +0100

    WIP.

--- src/messenger/message.util.ts
+++
@@ -1,5 +0,0 @@
-export function createMessage(message: string, temperature: string): string {
-  return encodeURI(
-    `${message}. Die aktuelle Wassertemperatur beträgt ${temperature} °C`,
-  );
-}
